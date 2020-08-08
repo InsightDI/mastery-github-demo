33db8bf1d7dfb0abbb8631a5ce4312ba57c18f7a
jgrizzle <james.grizzle@insight.com>
2020-08-08 12:14:17 -0400

    Added defaults

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -1,7 +1,25 @@
-variable "subscription_id" { type = string }
-variable "client_secret" { type = string }
-variable "client_id" { type = string }
-variable "tenant_id" { type = string }
+variable "tenant_id" {
+  type        = string
+  description = "The tenant to which the Terraform configuration will be deployed"
+  default     = "55a46e16-637a-4cfb-acaa-d89183887b16"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "The subscription to which the Terraform configuration will be deployed"
+  default     = "acd2e298-d61f-4ed2-8b28-dc2f60d9f382"
+}
+
+variable "client_id" {
+  type        = string
+  description = "The service principal ObjectId used to deploy the Terraform configuration"
+  default     = "e9933c22-daee-4e6e-b396-e89eb2feed93"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "The service principal client secret used to deploy the Terraform configuration"
+}
 
 variable "environment" {
   description = "The environment to which the Terraform configuration will be deployed"
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,3 @@
-# data "azurerm_client_config" "current" {}
-# data "azurerm_subscription" "primary" {}
-
 resource "azurerm_resource_group" "demo" {
   name     = "${var.environment}-${var.app_name}-rg"
   location = var.region_primary
