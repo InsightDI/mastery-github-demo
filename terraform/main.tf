c4fb3f31e84f763e6c6b263a69ed0d2fa3a15af5
jgrizzle <james.grizzle@insight.com>
2020-08-10 12:26:56 -0400

    updating tenant ID

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_key_vault" "demo" {
   name                = "mastery-demo-grizzle"
   location            = azurerm_resource_group.demo.location
   resource_group_name = azurerm_resource_group.demo.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  tenant_id           = var.tenant_id
 
   sku_name = "premium"
 
