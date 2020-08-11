30f722004f61c438dbf91d17447846a4eb4c741c
jgrizzle <james.grizzle@insight.com>
2020-08-11 12:58:56 -0400

    removed secrets

--- terraform/function-app.tf
+++ terraform/function-app.tf
@@ -36,7 +36,6 @@ module "function_app_listener" {
   appsettings = {
     "AzureWebJobsStorage" : "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.demo.name};SecretName=${azurerm_key_vault_secret.demo_storage_account_access_key.name};SecretVersion=${azurerm_key_vault_secret.demo_storage_account_access_key.version})",
     "FUNCTIONS_WORKER_RUNTIME" : "dotnet",
-    "devmasterysbusns_SERVICEBUS" : "Endpoint=sb://dev-mastery-sbus-ns.servicebus.windows.net/;SharedAccessKeyName=dev-mastery-owner-sbus-rule;SharedAccessKey=pyA6dZXK0/U347ZPaNmPgND+PlDsteJ7EsTigu7bwNk=;EntityPath=dev-mastery-sbus"
-    # "devmasterysbusns_SERVICEBUS": "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.demo.name};SecretName=${azurerm_key_vault_secret.demo_sbus_connection_string.name};SecretVersion=${azurerm_key_vault_secret.demo_sbus_connection_string.version})"
+    "devmasterysbusns_SERVICEBUS": "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.demo.name};SecretName=${azurerm_key_vault_secret.demo_sbus_connection_string.name};SecretVersion=${azurerm_key_vault_secret.demo_sbus_connection_string.version})"
   }
 }
